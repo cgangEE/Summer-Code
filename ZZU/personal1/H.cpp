fe6e54f9e15a4539f68e005b88cfb92d949aa361
cgangEE <cgangee@gmail.com>
2013-10-02 18:32:59 +0800

    write the meaning of the code and learn something...

---
+++ ZZU/personal1/H.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <cctype>
+#include <ctime>
+
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+using namespace std;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+typedef long long ll;
+template <class T> void checkmax(T &t, T x){if (x > t) t = x;}
+template <class T> void checkmin(T &t, T x){if (x < t) t = x;}
+template <class T> void _checkmax(T &t, T x){if (t == -1 || x > t) t = x;}
+template <class T> void _checkmin(T &t, T x){if (t == -1 || x < t) t = x;}
+#define INF (INT_MAX/10)
+#define SQR(x) ((x)*(x))
+#define rep(i, n) for (int i=0; i<(n); ++i)
+#define repf(i, a, b) for (int i=(a); i<=(b); ++i)
+#define repd(i, a, b) for (int i=(a); i>=(b); --i)
+#define iter(v) __typeof((v).begin())
+#define foreach(it, v) for (iter(v) it = (v).begin(); it != (v).end(); it++)
+#define clr(ar,val) memset(ar, val, sizeof(ar))
+#define pb push_back
+#define sz(a) ((int)(a).size())
+#define all(a) a.begin(), a.end()
+#define mid(x, y) ((x+y)/2)
+#define vp vector<P>
+#define itr iterator
+
+int i,j,k,m,n,l;
+vi a;
+int v[]={2, 3, 5, 7};
+
+void dfs(ll i, int j){
+	if (j==4){
+		a.pb(i);
+		return;
+	}
+	rep(k, 40){
+		dfs(i, j+1);
+		i*=v[j];
+		if (i>2000000000) break;
+	}
+}
+
+char *gao(int test){
+	if (test%100>=11 && test%100<=13) return "th";
+	if (test%10==1) return "st";
+	if (test%10==2) return "nd";
+	if (test%10==3) return "rd";
+	return "th";
+}
+
+int main(){
+	dfs(1, 0);
+	sort(all(a));
+	int test=0;
+	while (~scanf("%d", &n) && n){
+		++test;
+		printf("The %d%s humble number is %d.\n", n, gao(n), a[n-1]);
+	}
+	return 0;
+}
+
+
